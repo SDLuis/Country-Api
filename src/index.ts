ebfc57ef7979a591a6974e6232d2ee95f9b61290
Luis <Luistejeda0011@gmail.com>
2022-10-12 22:12:49 -0300

    working host

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import express from 'express'
 import countryRouter from './routes/country.routes'
 const app = express()
 app.use(express.json())
-
-const PORT = 5000
+/* eslint-disable */
+const PORT = process.env.PORT || 5000
 
 app.use('/country', countryRouter)
 
