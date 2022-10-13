df6dccc75bcb6574fa6d89f1e953e03e56d36d59
Luis <Luistejeda0011@gmail.com>
2022-10-13 00:30:12 -0300

    Adding pipeline with github actions

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import countryRouter from './routes/country.routes'
 const app = express()
 app.use(express.json())
 /* eslint-disable */
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use('/country', countryRouter)
+app.use('/country', countryRouter);
+
+app.get("/", (_req, res) => {
+  res.status(200).send("WELCOME!!");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 })
